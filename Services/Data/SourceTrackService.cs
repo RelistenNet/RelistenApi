7c25374d6795bff167aca57e0c68cdd20124fb51
Alec Gorge <alecgorge@gmail.com>
2016-12-13 10:12:59 -0500

    changes

--- Services/Data/SourceTrackService.cs
+++ Services/Data/SourceTrackService.cs
@@ -11,6 +11,18 @@ namespace Relisten.Data
     {
         public SourceTrackService(DbService db) : base(db) { }
 
+        public async Task<SourceTrack> ForId(int id)
+        {
+            return (await db.WithConnection(con => con.QueryAsync<SourceTrack>(@"
+                SELECT
+                    t.*
+                FROM
+                    source_tracks t
+                WHERE
+                    t.id = @trackId
+            ", new { trackId = id }))).FirstOrDefault();
+        }
+
         public async Task<IEnumerable<SourceTrack>> InsertAll(IEnumerable<SourceTrack> songs)
         {
             return await db.WithConnection(async con =>
