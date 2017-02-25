0865b43f27c0eb4b7f2abb02d69caac21d98c1fa
Alec Gorge <alecgorge@gmail.com>
2017-02-25 09:38:17 -0500

    add a queryr and update sources to have appropriate [Require] attributes

--- README.markdown
+++ README.markdown
@@ -7,3 +7,15 @@ pg_dump --no-acl --no-owner alecgorge_development --exclude-table="hangfire*" --
 
 pg_dump postgres:///alecgorge --data-only --inserts --no-owner --no-privileges -t artists -t features -t upstream_sources -t artists_upstream_sources > Data/base-data-init.sql
 ``
+
+# Delete all content for artist from a botched import
+
+```
+delete from setlist_songs where artist_id = 5;
+delete from setlist_shows where artist_id = 5;
+delete from shows where artist_id = 5;
+delete from sources where artist_id = 5;
+delete from tours where artist_id = 5;
+delete from venues where artist_id = 5;
+delete from years where artist_id = 5;
+```
--- RelistenApi/Models/Source.cs
+++ RelistenApi/Models/Source.cs
@@ -9,6 +9,7 @@ namespace Relisten.Api.Models
 {
     public class Source : BaseRelistenModel
     {
+		[Required]
         public int artist_id { get; set; }
 
         public int? show_id { get; set; }
@@ -17,31 +18,48 @@ namespace Relisten.Api.Models
         public int? venue_id { get; set; }
         public Venue venue { get; set; }
 
+		[Required]
         public string display_date { get; set; }
 
-        public bool is_soundboard { get; set; }
-        public bool is_remaster { get; set; }
+  		[Required]
+  	    public bool is_soundboard { get; set; }
+
+		[Required]
+		public bool is_remaster { get; set; }
+
+		[Required]
         public bool has_jamcharts { get; set; }
 
+		[Required]
         public double avg_rating { get; set; }
-        public int num_reviews { get; set; }
-        public int? num_ratings { get; set; }
-        public double avg_rating_weighted { get; set; }
-        public double duration { get; set; }
 
+		[Required]
+        public int num_reviews { get; set; }
+
+		public int? num_ratings { get; set; }
+        
+		[Required]
+		public double avg_rating_weighted { get; set; }
+
+		public double duration { get; set; }
+
+		[Required]
         public string upstream_identifier { get; set; }
-        public string description { get; set; }
+  
+		public string description { get; set; }
         public string taper_notes { get; set; }
         public string source { get; set; }
         public string taper { get; set; }
         public string transferrer { get; set; }
         public string lineage { get; set; }
-
     }
 
     public class SourceFull : Source
     {
+		[Required]
         public IList<SourceReview> reviews { get; set; }
-        public IList<SourceSet> sets { get; set; }
+  
+		[Required]
+		public IList<SourceSet> sets { get; set; }
     }
 }
