d6d4df61c95b1d0134b1356eba36dcf82f2adedd
Alec Gorge <alecgorge@gmail.com>
2018-01-20 13:07:21 -0800

    Fix sort on live. Sort by score not by rank

--- RelistenApi/Controllers/LiveController.cs
+++ RelistenApi/Controllers/LiveController.cs
@@ -16,21 +16,21 @@ namespace Relisten.Controllers
     {
         public ShowService _showService { get; set; }
         public SourceTrackService _sourceTrackService { get; set; }
-		public RedisService _redisService { get; set; }
+        public RedisService _redisService { get; set; }
 
-		readonly SourceService sourceService;
+        readonly SourceService sourceService;
 
-		public LiveController(
-			RedisService redis,
-			DbService db,
-			ArtistService artistService,
-			ShowService showService,
-			SourceService sourceService,
-			SourceTrackService sourceTrackService
-		) : base(redis, db, artistService)
-		{
-			this.sourceService = sourceService;
-			_redisService = redis;
+        public LiveController(
+            RedisService redis,
+            DbService db,
+            ArtistService artistService,
+            ShowService showService,
+            SourceService sourceService,
+            SourceTrackService sourceTrackService
+        ) : base(redis, db, artistService)
+        {
+            this.sourceService = sourceService;
+            _redisService = redis;
             _showService = showService;
             _sourceTrackService = sourceTrackService;
         }
@@ -39,46 +39,48 @@ namespace Relisten.Controllers
         [HttpPost("live/play")]
         [ProducesResponseType(typeof(bool), 200)]
         [ProducesResponseType(typeof(ResponseEnvelope<bool>), 404)]
-		public async Task<IActionResult> PlayedTrack([FromQuery] int track_id)
+        public async Task<IActionResult> PlayedTrack([FromQuery] int track_id)
         {
             var track = await _sourceTrackService.ForId(track_id);
 
-            if(track == null)
+            if (track == null)
             {
                 return JsonNotFound(false);
             }
 
-			await redis.db.SortedSetAddAsync("played", track_id, DateTimeOffset.UtcNow.ToUnixTimeSeconds());
+            await redis.db.SortedSetAddAsync("played", track_id, DateTimeOffset.UtcNow.ToUnixTimeSeconds());
 
             return JsonSuccess(true);
         }
 
-		[HttpPost("live/recently-played")]
-		[ProducesResponseType(typeof(IEnumerable<PlayedSourceTrack>), 200)]
-		public async Task<IActionResult> RecentlyPlayed()
-		{
-			var trackIds = (await redis.db.SortedSetRangeByRankAsync("played", -26, -1)).Select(t =>
-			{
-				t.TryParse(out int id);
-				return id;
-			});
+        [HttpPost("live/recently-played")]
+        [ProducesResponseType(typeof(IEnumerable<PlayedSourceTrack>), 200)]
+        public async Task<IActionResult> RecentlyPlayed()
+        {
+            var trackIds = (await redis.db
+                .SortedSetRangeByScoreAsync("played", start: -26, stop: -1, order: StackExchange.Redis.Order.Descending))
+                .Select(t =>
+            {
+                t.TryParse(out int id);
+                return id;
+            });
 
-			var tracks = await _sourceTrackService.ForIds(trackIds.ToList());
+            var tracks = await _sourceTrackService.ForIds(trackIds.ToList());
 
-			var sourceIds = tracks
-				.Select(t => t.source_id)
-				.GroupBy(t => t)
-				.Select(g => g.First());
+            var sourceIds = tracks
+                .Select(t => t.source_id)
+                .GroupBy(t => t)
+                .Select(g => g.First());
 
-			var sources = (await sourceService.SlimSourceWithShowAndArtistForIds(sourceIds.ToList()))
-				.GroupBy(s => s.id)
-				.ToDictionary(g => g.Key, g => g.First());
+            var sources = (await sourceService.SlimSourceWithShowAndArtistForIds(sourceIds.ToList()))
+                .GroupBy(s => s.id)
+                .ToDictionary(g => g.Key, g => g.First());
 
-			return JsonSuccess(tracks.Select(t => new PlayedSourceTrack
-			{
-				source = sources[t.source_id],
-				track = t
-			}));
-		}
+            return JsonSuccess(tracks.Select(t => new PlayedSourceTrack
+            {
+                source = sources[t.source_id],
+                track = t
+            }));
+        }
     }
 }
