e49ea5d32323e307513a3fc39ae3207c29f41647
Alec Gorge <alecgorge@gmail.com>
2018-10-23 15:28:30 -0700

    Fix prod

--- RelistenApi/Startup.cs
+++ RelistenApi/Startup.cs
@@ -197,7 +197,7 @@ namespace Relisten
 					migrator.Baseline(2);
 				}
 
-				migrator.MigrateTo(4);
+				migrator.Baseline(4);
 
 				if (migrator.LatestMigration.Version != migrator.CurrentMigration.Version)
 				{
