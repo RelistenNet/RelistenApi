fbf654e6e0df2ccfb8dad6c626f5c0dc31a5af75
Alec Gorge <alecgorge@gmail.com>
2019-01-06 10:43:16 -0800

    Fix migration #

--- RelistenApi/Migrations/05_AddSourceTrackPlaysIndex.cs
+++ RelistenApi/Migrations/05_AddSourceTrackPlaysIndex.cs
@@ -2,12 +2,15 @@ using SimpleMigrations;
 
 namespace Migrations
 {
-    [Migration(4, "Add source track plays index")]
+    [Migration(5, "Add source track plays index")]
     public class AddSourceTrackPlaysIndex : Migration
     {
         protected override void Up()
         {
-            Execute(@"CREATE INDEX IF NOT EXISTS idx_source_track_plays_id_btree ON source_track_plays(id);");
+            Execute(@"
+                CREATE INDEX IF NOT EXISTS idx_source_track_plays_id_btree ON source_track_plays(id);
+                CREATE INDEX CONCURRENTLY ON setlist_shows (artist_id);
+            ");
         }
 
         protected override void Down()
