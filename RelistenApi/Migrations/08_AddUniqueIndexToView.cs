a38a390f0eee3a0a635bbf6aea220332cb074e03
Alec Gorge <alecgorge@gmail.com>
2025-12-21 15:13:21 -0600

    add recent plays index

---
+++ RelistenApi/Migrations/08_AddUniqueIndexToView.cs
@@ -0,0 +1,24 @@
+using SimpleMigrations;
+
+namespace Migrations;
+
+[Migration(8, "Add unique index to source_track_plays_by_day_6mo")]
+public class AddUniqueIndexToView: Migration {
+    public AddUniqueIndexToView()
+    {
+        this.UseTransaction = false;
+    }
+    
+    protected override void Up()
+    {
+        // In practice, there won't be duplicates but we don't need to enforce at the DB level
+        Execute(@"
+            CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS ""source_track_plays_by_day_6mo_source_track_uuid_play_day_idx"" ON ""source_track_plays_by_day_6mo""(""source_track_uuid"",""play_day"");
+        ");
+    }
+
+    protected override void Down()
+    {
+        throw new System.NotImplementedException();
+    }
+}
